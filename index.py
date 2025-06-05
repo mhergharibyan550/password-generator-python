d6e64ab8fc7b41c798b48074fe5dacf4b3a210c2
mhergharibyan550 <mhergharibyan550@gmail.com>
2025-06-05 16:23:11 +0400

    Finished password generator

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 from tools import generate_password
 
-generate_password()
+
+password_length = int(input("Input password length: "))
+password = generate_password(password_length)
+
+print(password)
--- tools.py
+++ tools.py
@@ -1,14 +1,19 @@
 from random import randint
 
 
-def generate_password():
-    password_length = int(input("Input password length: "))
+def generate_password(password_length: int):
     assert isinstance(password_length, int), "Password should be a valid number"
+    assert password_length > 5, "Password length must be at least 6 characters"
 
     password_char_list = []
 
     for i in range(password_length):
-        ascii_decimal_char = randint(0, 127)
-        password_char_list.append(chr(ascii_decimal_char))
+        isdigit = True if randint(0, 1) == 1 else False
 
-    return str(password_char_list)
+        if isdigit:
+            password_char_list.append(str(randint(0, 9)))
+        else:
+            ascii_decimal_char = randint(33, 126)
+            password_char_list.append(chr(ascii_decimal_char))
+
+    return "".join(password_char_list)
